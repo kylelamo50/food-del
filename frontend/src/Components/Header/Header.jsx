ef3b5507b61bbbfc5f2fe4a0ec500f20c0fe32fe
kylelamo50@gmail.com <kylelamo50@gmail.com>
2024-11-26 20:50:13 -0400

    First commit

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order Your Favourite Food Here</h2>
+            <p>kofdkbokokdpobkrdkopkokk fbrropropbpo bkboprophro o rbjorjorjboprjoj onjorjorjorhjohrj nojrohjohrjohrpjorhj ojrohjohrjorhj rhorjojrhophjrop onrjohrjophrjophrj</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  return (
+
+    
+    
+    <div>  
+        <Header/>   {/* mout header  in Home*/}
+        <ExploreMenu/>
+    </div>
+  )
+}
+
+export default Home
